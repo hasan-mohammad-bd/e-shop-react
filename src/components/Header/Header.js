ad7cddbed1e14dd3ee87bdbffb704f8906609e28
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-04-03 11:37:05 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import './Header.css';
+
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="img-holder">
+                <img src="../../images/Logo.svg" alt="" />
+            </div>
+            <nav>
+                
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/ManageInventory/ManageInventory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ManageInventory.css'
+
+const ManageInventory = () => {
+    return (
+        <div>
+            <h2>This is manage inventory section</h2>
+        </div>
+    );
+};
+
+export default ManageInventory;
